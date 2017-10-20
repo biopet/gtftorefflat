9e1728d128abd9b73184ac334f20cc1c176c5db6
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-10-20 15:49:50 +0200

    inputs and outputs where switched

--- src/main/scala/nl/biopet/tools/gtftorefflat/ArgsParser.scala
+++ src/main/scala/nl/biopet/tools/gtftorefflat/ArgsParser.scala
@@ -7,10 +7,10 @@ import nl.biopet.utils.tool.AbstractOptParser
 class ArgsParser(cmdName: String) extends AbstractOptParser[Args](cmdName) {
   opt[File]('r', "refFlat") required () unbounded () valueName "<file>" action { (x, c) =>
     c.copy(refFlat = x)
-  } text "Input refFlat file. Mandatory"
+  } text "Output refFlat file. Mandatory"
   opt[File]('g', "gtfFile") required () unbounded () valueName "<file>" action { (x, c) =>
     c.copy(gtfFile = x)
-  } text "Output gtf file. Mandatory"
+  } text "Input gtf file. Mandatory"
   opt[File]('R', "referenceFasta") unbounded () valueName "<file>" action { (x, c) =>
     c.copy(referenceFasta = Some(x))
   } text "Reference file"
